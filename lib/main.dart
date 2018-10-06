5a7e8e0227a8c4953cdc1554815a4f2e6ade2be5
Viktor Rocha <viktoorrocha@gmail.com>
2018-10-06 18:53:44 -0400

    ScrollView and RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main () {
-
+void main() {
   runApp(MaterialApp(
     home: Home(),
-
   ));
-
 }
 
 class Home extends StatefulWidget {
@@ -23,10 +20,52 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.green,
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.refresh),
-          onPressed: () {},)
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          )
         ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person, size: 120.0, color: Colors.green),
+            TextField(keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Weight (kg)",
+                  labelStyle: TextStyle(color: Colors.green)
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+            ),
+            TextField(keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Height (cm)",
+                  labelStyle: TextStyle(color: Colors.green)
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child:  Container(
+                height: 50.0,
+                child:  RaisedButton(
+                  onPressed: () {},
+                  child: Text("Calculate", style: TextStyle(color: Colors.white, fontSize: 25.0),),
+                  color: Colors.green,
+                ),
+              ),
+            ),
 
+            Text("Info",
+              textAlign:  TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),)
+          ],
+        ),
       ),
     );
   }
